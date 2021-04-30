07ffc91d45d53fc3fc9a3d84888c28a41c2b1403
VVVVVVVIN <77380046+VVVVVVVIN@users.noreply.github.com>
2021-05-01 01:22:52 +0900

    #0.13 - 210501

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,7 @@
-import { getMovies, getById, addMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
   },
 };
-
-export default resolvers;
